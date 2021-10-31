6c0921f1836ff935ca3ffaf28792576b208457fa
Thomas Artmann <thwohmas@gmail.com>
2021-10-31 03:57:53 +0100

    implemented footer

---
+++ components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class='footer'>
+    <hr>
+    <NuxtLink to="/hello">Was ist BigCity</NuxtLink>
+    <NuxtLink to="/impressum">Impressum</NuxtLink>
+    <NuxtLink to="/datenschutz">Datenschutz</NuxtLink>
+  </div>
+</template>
