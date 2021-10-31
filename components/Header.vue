5320c2a516ca3750abadf7fa40c5ca17d5aae6f9
Thomas Artmann <thwohmas@gmail.com>
2021-10-31 03:57:59 +0100

    implemented header

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='header'>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="/">BigCity</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right text="User">
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
