61facbb1abe2d605527b9118c5e542a9b4373eaa
Thomas Artmann <artmann@die-etagen.de>
2021-11-05 16:51:02 +0100

    implemented headline component which automatically adds a slug as id

---
+++ components/elements/Headline.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1 v-if="level === '1'" :id='slug' class="headline headline-1">{{ content }}</h1>
+  <h2 v-else-if="level === '2'" :id='slug' class="headline headline-2">{{ content }}</h2>
+  <h3 v-else-if="level === '3'" :id='slug' class="headline headline-3">{{ content }}</h3>
+  <h4 v-else-if="level === '4'" :id='slug' class="headline headline-4">{{ content }}</h4>
+  <h5 v-else-if="level === '5'" :id='slug' class="headline headline-5">{{ content }}</h5>
+  <h6 v-else-if="level === '6'" :id='slug' class="headline headline-6">{{ content }}</h6>
+</template>
+
+<script>
+export default {
+  name: "Headline",
+  props: {
+    level: {
+      type: String,
+      default: "2"
+    },
+    content: {
+      type: String,
+      default: "",
+    }
+  },
+  data() {
+    return {
+      slug: ''
+    }
+  },
+  created() {
+    this.slug = this.content
+      .toLowerCase()
+      .replaceAll('ä', 'ae')
+      .replaceAll('ö', 'oe')
+      .replaceAll('ü', 'ue')
+      .replaceAll('ß', 'ss')
+      .replaceAll(' ', '-')
+      .replaceAll('_', '-')
+      .replaceAll(/^\W.+$/g, '')
+    ;
+  }
+}
+</script>
