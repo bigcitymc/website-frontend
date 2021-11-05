f1e5e3be668c505b163049896953b4a9d5044a93
Thomas Artmann <artmann@die-etagen.de>
2021-11-05 16:51:43 +0100

    implemented post base and text post components

---
+++ components/post/PostBase.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <b-card header-tag="header" footer-tag="footer">
+      <template #header>
+        <b-img src="https://picsum.photos/16/16/?image=25"></b-img>
+        <NuxtLink to="#">Username</NuxtLink>
+        <time class="">05.11.2021 3:01</time>
+      </template>
+      <slot>PostBase</slot>
+      <template #footer>
+        <b-button-group>
+          <b-dropdown split text="Add Reaction" variant="primary">
+            <b-dropdown-item>Like</b-dropdown-item>
+            <b-dropdown-item>Dislike</b-dropdown-item>
+          </b-dropdown>
+        </b-button-group>
+        <b-button-group>
+          <b-button href="#" variant="primary">Report</b-button>
+        </b-button-group>
+      </template>
+    </b-card>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "PostBase",
+}
+</script>
---
+++ components/post/TextPost.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <PostBase>
+      <b-card-text>{{ text }}</b-card-text>
+    </PostBase>
+  </main>
+</template>
+
+<script>
+import PostBase from "~/components/post/PostBase";
+export default {
+  name: "TextPost",
+  components: {PostBase},
+  props: {
+    text: {
+      type: String,
+      default: ""
+    }
+  }
+}
+</script>
